e0b04e31bc098dfd1987a3262901a7eadfbf6670
SANjivSs <ss8845548@gmail.com>
2025-02-12 12:59:42 +0530

    created a new file

---
+++ shift_operator.java
@@ -0,0 +1,10 @@
+public class shift_operator {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 2;
+        int c = a << b;
+        int d = a >> b;
+        System.out.println("Value of c is: "+c);
+        System.out.println("Value of d is: "+d);
+    }
+}
