9cc3f87352bc885f4d71c346c7e6d43a4c3fe3b9
SANjivSs <ss8845548@gmail.com>
2025-02-12 11:51:03 +0530

    created a new file

---
+++ logical_operator.java
@@ -0,0 +1,18 @@
+public class logical_operator {
+    public static void main(String[] args) {
+        int a =100;
+        int b =20;
+        int c =30;
+if(a>b && a>c){
+
+            System.out.println("a is greater than b and c");
+        }
+else if(b<a & b<c){
+
+            System.out.println("b is greater than a and c");
+        }
+        else{
+            System.out.println("c is greater than a and b");
+        }
+}
+}
