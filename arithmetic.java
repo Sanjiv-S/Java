3b6f72a4df4b1b394e483a52127c3a2762be16c3
SANjivSs <ss8845548@gmail.com>
2025-02-12 11:12:39 +0530

    created a new file

---
+++ arithmetic.java
@@ -0,0 +1,16 @@
+public class arithmetic {
+    public static void main (String args[]){
+        byte a =100;
+        byte b =98;
+        byte add=(byte)(a+b);
+        byte sub=(byte)(a-b);
+        byte mul=(byte)(a*b);
+        byte div=(byte)(a/b);
+        byte mod=(byte)(a%b);
+        System.out.println("sum of the numbers:"+add);
+        System.out.println("sub of the numbers:"+sub);
+        System.out.println("mul of the numbers:"+mul);
+        System.out.println("div of the numbers:"+div);
+        System.out.println("mod of the numbers:"+mod);
+    }
+}
