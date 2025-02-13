aa854dfb52314f1e036b39f3f735aeaee4df4768
SANjivSs <ss8845548@gmail.com>
2025-02-13 21:04:44 +0530

    updated a file

--- diamond_pattern.java
+++ diamond_pattern.java
@@ -28,7 +28,7 @@ public class diamond_pattern {
             }
             System.out.println(" ");
         }*/
-        int n = 4; 
+       /*  int n = 4; 
 
         
         for (int i = 1; i <= n; i++) {
@@ -54,5 +54,39 @@ public class diamond_pattern {
             System.out.println();
         }
 
+    }*/
+    int n = 4; 
+
+        
+    for (int i = 1; i <= n; i++) {
+       
+        for (int j = n; j > i; j--) {
+            System.out.print(" ");
+        }
+       
+        for (int k = 1; k <= (2 * i - 1); k++) {
+            if(k==1 || k==(2*i-1)){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+            
+        }
+        System.out.println();
+    }
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = n; j > i; j--) {
+            System.out.print(" ");
+        }
+        for (int k = 1; k <= (2 * i - 1); k++) {
+            if (k == 1 || k == (2 * i - 1)) {
+                System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
     }
 }
