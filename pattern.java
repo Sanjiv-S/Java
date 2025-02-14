4bd15739e5d735966619315ca70049aa4901db5e
SANjivSs <ss8845548@gmail.com>
2025-02-14 16:35:32 +0530

    Added methods to the program

--- pattern.java
+++ pattern.java
@@ -1,5 +1,5 @@
 public record pattern() {
-    public static void main (String args[]){
+    public void zero_pyramid(){
         int i;
         int j;
         int n2=4;
@@ -25,5 +25,33 @@ public record pattern() {
             }                                                                                                                                                                                                                                       
             q++;
         }
+
+    }
+    public void num_pyramid(){
+        int n=4;
+        int count =1;
+        for(int i =1;i<=n;i++){
+            for(int j =1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k =1;k<=(2*i-1); k++){
+                if(k==1 || k ==(2*i-1) || i == n){
+                    System.out.print("*");
+                }
+                else{
+                    while(k==(2*i-1)){
+                        System.out.print(count);
+                        count++;
+                    }
+                }
+            }
+            System.out.println("");
+        }
+    }
+    public static void main (String args[]){
+       pattern pat = new pattern();
+       pat.zero_pyramid();
+       System.out.println("");
+       pat.num_pyramid();
     }
 }
