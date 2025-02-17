1809cbca6e32dc009d9479c4532bbdbadd72336f
SANjivSs <ss8845548@gmail.com>
2025-02-17 12:40:56 +0530

    created a new file

--- zero_at_last.java
+++ zero_at_last.java
@@ -15,9 +15,27 @@ public class zero_at_last {
             System.out.print(arr[k] + " ");
         }
     }
+    public void zero_one(){
+        int arr[] = {1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1};
+        int count = 0; 
+       
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[count++] = arr[i];
+            }
+        }
+        while (count < arr.length) {
+            arr[count++] = 0;
+        }
+        for (int k = 0; k < arr.length; k++) {
+            System.out.print(arr[k] + " ");
+        }
+    }
     
     public static void main(String[] args) {
         zero_at_last obj =new zero_at_last();
         obj.zero_at_lasts(); 
+        System.out.println("");
+        obj.zero_one();
     } 
 }
