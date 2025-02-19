5102bcb4ac68d7655e7e503b67bd598fd3e20771
SANjivSs <ss8845548@gmail.com>
2025-02-19 16:17:30 +0530

    Created a new file

--- transpose.java
+++ transpose.java
@@ -1,6 +1,6 @@
 public class transpose {
-    public static void main(String args[]){
-        int arr[][]={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+    public void transpose_array(){
+        int arr[][]={{1,2,3,5},{4,5,6,11},{7,8,9,22},{66,78,9,54}};
         int row_start=0;
         int col_start=0;
         int row_end=arr.length;
@@ -18,5 +18,32 @@ public class transpose {
             }
             System.out.println(); 
         }
+    }
+    public void add_element(){
+        int arr[][]={
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        int n =arr.length;
+        int c[]=new int[5];
+        int k=0;
+        int sum=0;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                    c[k]=arr[i][j];
+                    k++;
+                    }    
+                }
+        for (int i = 0; i < k; i++) {
+            sum += c[i];
+        }
+        System.out.print(sum);
+    }
+    public static void main(String args[]){
+        transpose obj =new transpose();
+        obj.transpose_array();
+        System.out.println();
+        obj.add_element();
     }
 }
