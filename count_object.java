18bffbcd74bea1369224fb5b466d781f9cbcc6af
SANjivSs <ss8845548@gmail.com>
2025-02-21 15:07:17 +0530

    created a new file

---
+++ count_object.java
@@ -0,0 +1,13 @@
+public class count_object {
+    int count=0;
+    public static void main(String args []){
+        counts();
+       System.out.print("inside main method");
+    }
+    
+    static void counts(){
+       System.out.print("inside static method");
+       
+      
+    }
+}
---
+++ static_pattern.java
@@ -0,0 +1,22 @@
+public class static_pattern {
+    static int n=5;
+        static void pattern(){
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=i;j++){
+                    if(j%2 == 0){
+                        System.out.print("#");
+                    }
+                    else{
+                        System.out.print("*");
+                    }
+                }
+                System.out.println();
+            }
+            
+        }
+    
+    public static void main(String args[]){
+        pattern();
+    }
+}
+
