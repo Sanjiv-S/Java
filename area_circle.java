b6ffdedc44138ad8bf3b760d53c02c70ccb7d813
SANjivSs <ss8845548@gmail.com>
2025-02-24 10:00:44 +0530

    created a new file

---
+++ area_circle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+abstract class abstract_class {
+    protected double area;
+    abstract void calc_area();
+}
+class area_circle extends abstract_class{
+    private double r;
+    public void Area(){
+        Scanner in =new Scanner(System.in);
+        System.out.print("Enter a value:");
+        r=in.nextDouble();
+    }
+    void calc_area(){
+        area =3.14*r*r;
+    System.out.println("Area="+area);
+    }
+    public static void main(String args[]){
+        area_circle obj = new area_circle();
+        obj.Area();
+        obj.calc_area();
+    }
+}
+
