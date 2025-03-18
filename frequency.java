039d724270bf8420b82c43d7776bb3ddc980056c
SANjivSs <ss8845548@gmail.com>
2025-03-18 11:44:15 +0530

    created a new file

--- frequency.java
+++ frequency.java
@@ -1,6 +1,7 @@
+import java.util.HashMap;
+
 public class frequency {
-    
-    public static void main(String args[]){
+    static void sort(){
         int arr[] ={10,5,4,2};
         int n=arr.length;
         int temp;
@@ -17,5 +18,22 @@ public class frequency {
         for(int i:arr){
             System.out.print(i+" ");
         }
+    }
+    static void freq(){
+        String fre = "Thunderthun";
+        char arr[] = fre.toCharArray();
+        int n = arr.length;
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+        for (char c : arr) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+        for (char key : freqMap.keySet()) {
+            System.out.println(key + " -> " + freqMap.get(key));
+        }
+    }
+    public static void main(String args[]){
+       sort();
+       System.out.println(); 
+       freq();
     }
 }
